2c4ea9795987964ef8af504469e76d04e56ee097
jose <joseluissanchez41@hotmail.com>
2021-02-04 12:04:02 -0500

    cambios en el js pedidos

--- public/js/adminlte/pedidos/pedidos.js
+++ public/js/adminlte/pedidos/pedidos.js
@@ -88,7 +88,7 @@ $(document).ready(function () {
 
     $(document).on("click", "#btn-buscarpersona",function(){
       
-      $('.loaders').removeClass('d-none');
+     // $('.loaders').removeClass('d-none');
         cargarClientes();
    
         $("#modal-buscarpersona").modal("show");

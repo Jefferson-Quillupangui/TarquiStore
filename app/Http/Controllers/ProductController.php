213f474a0352dcac134de1ceec1bba1a322f5f47
jose <joseluissanchez41@hotmail.com>
2021-02-28 21:36:40 -0500

    v3

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -77,7 +77,8 @@ class ProductController extends Controller
         //Subida de imagen mediante intervention image pck
         $nombre = Str::random(10).$request->file('image')->getClientOriginalName();
 
-        $ruta = '\app\public\img/'.$nombre;
+        //$ruta = storage_path().'\app\public\img/'.$nombre;
+        $ruta = public_path (). '/public_html/img/'.$nombre;
 
         Image::make($request->file('image'))
                 ->resize(1200, null, function ($constraint) {
